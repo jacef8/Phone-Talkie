7a0add7f8e2de6c1eab80ea29e3665484d37959f
jacef8 <jaceford08@gmail.com>
2026-06-10 23:07:42 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Unregister this service worker completely
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
